a61cc76818557427cb73fe2d0b5c9e7d823a3169
marcus <manhcuong313.ng@gmail.com>
2025-04-04 17:21:06 +0700

    update ode45 dynamic model

--- robot_model/manipulator_dynamics_ode45.m
+++ robot_model/manipulator_dynamics_ode45.m
@@ -1,15 +1,11 @@
-function dxdt = manipulator_dynamics_ode45(t, x, t_ref, q_d_ref, q_d_dot_ref, q_d_ddot_ref, controller, param, dt_sim)
+function dxdt = manipulator_dynamics_ode45(t, x, tau, noisy_force, use_potential_force, param)
     % ODE Function for manipulator dynamics
     % Inputs:
     %   t - Current time
     %   x - Current state vector [q; q_dot]
-    %   t_ref - Reference time vector
-    %   q_d_ref - Reference joint positions
-    %   q_d_dot_ref - Reference joint velocities
-    %   q_d_ddot_ref - Reference joint accelerations
-    %   controller - Controller object
+    %   tau - Torque input
+    %   noisy_force - Disturbance force
     %   param - Robot parameters
-    %   dt_sim - Simulation time step
     % Outputs:
     %   dxdt - State derivative vector [q_dot; q_ddot]
     
@@ -17,25 +13,16 @@ function dxdt = manipulator_dynamics_ode45(t, x, t_ref, q_d_ref, q_d_dot_ref, q_
     q = x(1:2);
     q_dot = x(3:4);
     
-    % Find the closest index in the reference time vector
-    [~, idx] = min(abs(t_ref - t));
-    
-    % Get reference values at the closest time point
-    q_d = q_d_ref(:,idx);
-    q_d_dot = q_d_dot_ref(:,idx);
-    q_d_ddot = q_d_ddot_ref(:,idx);
     
     % Compute nominal model dynamics (without disturbance)
     [M, C, G, B] = manipulator_dynamics(q, q_dot, param.m1, param.m2, param.l1, param.l2, param.lc1, param.lc2, param.I1, param.I2, param.g, 0);
     
-    % Compute true model dynamics with uncertainties
-    [M_true, C_true, G_true, B] = manipulator_dynamics(q, q_dot, param.m1, param.m2, param.l1, param.l2, param.lc1, param.lc2, param.I1, param.I2, param.g, noisy_force);
-    
-    % Compute control signal
-    [tau, ~, ~, ~, ~] = controller.computeControl(q, q_dot, q_d, q_d_dot, q_d_ddot, M, C, G, B, dt_sim);
-    
+    if use_potential_force
     % Compute acceleration using the true model dynamics
-    q_ddot = M_true \ (tau - C_true * q_dot - G_true);
+        q_ddot = M \ (tau - C * q_dot - G + noisy_force);
+    else
+        q_ddot = M \ (tau - C * q_dot);
+    end
     
     % Return the state derivatives
     dxdt = [q_dot; q_ddot];
